34e4cd9b282b01803ebde87be31c0adfd3ee0781
Luke Donovan <lukelamnisos@icloud.com>
2022-02-05 11:17:51 +0000

    configured test file

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,24 @@
-// contract test code will go here
+const { equal } = require('assert');
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider())
+const {interface, bytecode} = require('../compile');
+
+let accounts
+let inbox
+
+beforeEach(async () => {
+  //get a list of all accounts
+  accounts = await web3.eth.getAccounts()
+  //use one of these accounts to deploy the smart contract to
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ["Hi there!"] })
+    .send({ from: accounts[0], gas: '1000000' });
+})
+
+describe('Contract', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address)
+  });
+});
